7fa901bf00a031964671bd12944b6013cce46cf3
clarkngo <clarkngo@gmail.com>
2021-01-10 16:25:44 -0800

    change connection string variable

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
